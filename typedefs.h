3894a46216065261d0274727d0ad7a9d30e611aa
Sebastian Alff <donalffons@gmail.com>
2020-04-18 22:57:33 +0200

    more api parts exposed

--- typedefs.h
+++ typedefs.h
@@ -1,6 +1,11 @@
 #include <Standard_Handle.hxx>
 #include <Poly_Triangulation.hxx>
 #include <TColStd_HSequenceOfTransient.hxx>
+#include <XSControl_WorkSession.hxx>
+#include <Transfer_TransientProcess.hxx>
 
 typedef Handle(Poly_Triangulation) Handle_Poly_Triangulation;
 typedef Handle(TColStd_HSequenceOfTransient) Handle_TColStd_HSequenceOfTransient;
+typedef Handle(XSControl_WorkSession) Handle_XSControl_WorkSession;
+typedef Handle(Transfer_TransientProcess) Handle_Tansfer_TransientProcess;
+typedef Handle(Message_ProgressIndicator) Handle_Message_ProgressIndicator;
