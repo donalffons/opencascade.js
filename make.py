1430a15ede2b348f58373035d0e0ea42e1c65e57
Sebastian Alff <donalffons@gmail.com>
2020-05-10 12:02:21 +0200

    fix build

--- make.py
+++ make.py
@@ -272,7 +272,9 @@ def build():
   open(temp, 'w').write(wrapped)
 
   os.chdir('..')
+  if not wasm:
   shutil.copyfile(os.path.join('build', 'js', 'opencascade.js'), os.path.join('dist', 'opencascade.js'))
+  else:
   shutil.copyfile(os.path.join('build', 'js', 'opencascade.wasm.js'), os.path.join('dist', 'opencascade.wasm.js'))
   shutil.copyfile(os.path.join('build', 'js', 'opencascade.wasm.wasm'), os.path.join('dist', 'opencascade.wasm.wasm'))
 
