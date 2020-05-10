1162b59f20d66aac5c5daf65a642dd54231cbc6f
Sebastian Alff <donalffons@gmail.com>
2020-05-10 12:03:17 +0200

    add missing typedefs

--- typedefs.h
+++ typedefs.h
@@ -5,6 +5,9 @@
 #include <Transfer_TransientProcess.hxx>
 #include <Poly_Polygon3D.hxx>
 #include <Geom_Curve.hxx>
+#include <Geom_TrimmedCurve.hxx>
+#include <Geom_Surface.hxx>
+#include <Standard_Type.hxx>
 
 typedef Handle(Poly_Triangulation) Handle_Poly_Triangulation;
 typedef Handle(TColStd_HSequenceOfTransient) Handle_TColStd_HSequenceOfTransient;
@@ -13,3 +16,7 @@ typedef Handle(Transfer_TransientProcess) Handle_Tansfer_TransientProcess;
 typedef Handle(Message_ProgressIndicator) Handle_Message_ProgressIndicator;
 typedef Handle(Poly_Polygon3D) Handle_Poly_Polygon3D;
 typedef Handle(Geom_Curve) Handle_Geom_Curve;
+typedef Handle(Geom_TrimmedCurve) Handle_Geom_TrimmedCurve;
+typedef Handle(Geom_Surface) Handle_Geom_Surface;
+typedef Handle(Standard_Type) Handle_Standard_Type;
+typedef Handle(Geom_Plane) Handle_Geom_Plane;
