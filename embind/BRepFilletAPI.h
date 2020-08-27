d9e0ea27207cbfeeb31de934e9a7465d8a379242
Sebastian Alff <donalffons@gmail.com>
2020-08-27 16:27:26 +0200

    more bindings, more issues...

--- embind/BRepFilletAPI.h
+++ embind/BRepFilletAPI.h
@@ -1,4 +1,4 @@
-#include ".BRepFilletAPI/BRepFilletAPI_LocalOperation.h"
-#include ".BRepFilletAPI/BRepFilletAPI_MakeChamfer.h"
-#include ".BRepFilletAPI/BRepFilletAPI_MakeFillet2d.h"
-#include ".BRepFilletAPI/BRepFilletAPI_MakeFillet.h"
+#include "./BRepFilletAPI/BRepFilletAPI_LocalOperation.h"
+#include "./BRepFilletAPI/BRepFilletAPI_MakeChamfer.h"
+#include "./BRepFilletAPI/BRepFilletAPI_MakeFillet2d.h"
+#include "./BRepFilletAPI/BRepFilletAPI_MakeFillet.h"
--- embind/TopExp.h
+++ embind/TopExp.h
@@ -1,3 +1,3 @@
-#include "./TopExp/TopExp_Explorer.h"
-#include "./TopExp/TopExp.h"
+// #include "./TopExp/TopExp_Explorer.h"
+// #include "./TopExp/TopExp.h"
 #include "./TopExp/TopExp_Stack.h"
