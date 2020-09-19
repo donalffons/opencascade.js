289872bf8c9b79b21d9aebb16267e863ed5a8aaf
Sebastian Alff <donalffons@gmail.com>
2020-09-19 12:11:11 +0200

    fix output doc

--- embind/autobind.py
+++ embind/autobind.py
@@ -806,7 +806,7 @@ def main():
 
 This is an overview over the currently supported parts of the OpenCascade API. Supported items are marked in green. Unsupported items are marked in red. All bindings listed here are created automatically. We are working hard on increasing support for as much as possible of the OpenCascade API.
 
-This list currently only lists simple class definitions and methods. It does not contain information about:
+This list only lists class definitions. It does not contain information about:
 * Nested class definitions. Those are currently not supported.
 * Overloaded constructors. These are generally well supported.
 * `typedef`'d template classes. Currently, we support most of the `Handle_`-types (i.e. `opencascade::handle<...>`. Apart from that, there is little support.
