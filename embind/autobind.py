353d5f43c2e2b6c9ff2f70e7129095548dbfcf49
Sebastian Alff <donalffons@gmail.com>
2020-09-19 21:52:54 +0200

    reduced build for debugging

--- embind/autobind.py
+++ embind/autobind.py
@@ -56,6 +56,7 @@ def processIncludeFile(filename):
 # returns:
 #   bool
 def processClass(theClass):
+  return False
   # error: undefined symbol: FT_Done_Face and many more
   if theClass.spelling.startswith("F") :
     return False
@@ -842,8 +843,8 @@ using namespace emscripten;
   newChildren = sorted(newChildren, key=lambda x: x.spelling)
 
   epilog = generateClassBindings(newChildren, outputFile, outputDocFile)
-  generateHandleTypeBindings(outputFile, children)
-  generateEnumBindings(newChildren, outputFile)
+  # generateHandleTypeBindings(outputFile, children)
+  # generateEnumBindings(newChildren, outputFile)
 
   outputFile.write("}" + os.linesep + os.linesep)
   outputFile.write(epilog)
