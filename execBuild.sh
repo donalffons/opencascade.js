2c5d71a5f70090c3543b23928e7493ecabd2b25f
Sebastian Alff <donalffons@gmail.com>
2020-09-22 19:35:11 +0200

    try killing everything after build

--- execBuild.sh
+++ execBuild.sh
@@ -4,4 +4,6 @@ source /emscripten/emsdk_env.sh
 
 ./patience.sh &
 python3.8 make.py wasm > /dev/null 2>&1
-echo "done."
+echo "done, killing everything"
+
+ps aux | tail -n +2 | egrep -v "docker-init" | cut -b12-17 | xargs -t kill -9
