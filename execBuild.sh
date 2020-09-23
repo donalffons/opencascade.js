64790d2c709d2d9fac52af0efa8eb4e33b497e9c
Sebastian Alff <donalffons@gmail.com>
2020-09-23 05:37:43 +0200

    just another attempt to fix build

--- execBuild.sh
+++ execBuild.sh
@@ -2,8 +2,4 @@
 
 source /emscripten/emsdk_env.sh
 
-./patience.sh &
-python3.8 make.py wasm > /dev/null 2>&1
-echo "done, killing everything"
-
-ps aux | tail -n +2 | egrep -v "docker-init" | cut -b12-17 | xargs -t kill -9
+python3.8 make.py wasm
