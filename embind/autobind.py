314594019426f778bcec3cce61e6eed4b1a332b9
Sebastian Alff <donalffons@gmail.com>
2020-09-25 18:29:53 +0200

    relaxed filtering for occt include files

--- embind/autobind.py
+++ embind/autobind.py
@@ -26,27 +26,103 @@ class MultipleBaseClassException(Exception):
 # returns:
 #   bool
 def processIncludeFile(filename):
+  if not filename.endswith(".hxx"):
+    return False
+
+  # fatal error: 'AIS_LocalStatus.hxx' file not found
   if (
-    filename.endswith(".hxx")
-    and not filename.startswith("IVtk")
-    and not filename.startswith("vtk")
-    and not filename.startswith("Interface")
-    and not filename.startswith("Xw")
-    and not filename.startswith("OSD")
-    and not filename.startswith("BVH")
-    and not filename.startswith("WNT")
-    and not filename.startswith("WNT")
-    and not filename.startswith("BOPTools")
-    and not filename.startswith("BOPDS")
-    and not filename.startswith("IntPatch")
-    and not filename.startswith("OpenGl")
-    and not filename.startswith("AIS")
-    and not filename.startswith("D3D")
-    and not filename.startswith("Aspect")
-    and not filename.startswith("Standard_Atomic")
+    filename == "AIS_DataMapOfSelStat.hxx" or
+    filename == "AIS_DataMapIteratorOfDataMapOfSelStat.hxx"
   ):
-    return True
-  return False
+    return False
+
+  # fatal error: 'X11/Xlib.h' file not found
+  if (
+    filename == "InterfaceGraphic.hxx"
+  ):
+    return False
+
+  # fatal error: 'X11/XWDFile.h' file not found
+  if filename == "Aspect_XWD.hxx":
+    return False
+
+  # fatal error: 'X11/Shell.h' file not found
+  if filename == "IVtkDraw_Interactor.hxx":
+    return False
+
+  # error: use of undeclared identifier 'myBoxes' / error: use of undeclared identifier 'myElements'
+  if filename == "BVH_IndexedBoxSet.hxx":
+    return False
+
+  # error: "Atomic operation isn't implemented for current platform!"
+  if (
+    filename == "BOPDS_Iterator.hxx" or
+    filename == "BOPDS_IteratorSI.hxx" or
+    filename == "BOPTools_BoxTree" or
+    filename == "BOPTools_BoxTree.hxx" or
+    filename == "BVH_LinearBuilder.hxx" or
+    filename == "BVH_RadixSorter.hxx" or
+    filename == "OSD_Parallel.hxx" or
+    filename == "OSD_ThreadPool.hxx" or
+    filename == "Standard_Atomic.hxx" or
+    filename == "BOPTools_Parallel.hxx" or
+    filename == "BVH_DistanceField.hxx"
+  ):
+    return False
+
+  # error: no member named 'random_shuffle' in namespace 'std'
+  if (
+    filename == "OpenGl_HaltonSampler.hxx" or
+    filename == "OpenGl_TileSampler.hxx" or
+    filename == "D3DHost_View.hxx" or
+    filename == "OpenGl_View.hxx"
+  ):
+    return False
+
+  # fatal error: 'vtkType.h' file not found
+  if (
+    filename == "IVtk_Types.hxx" or
+    filename == "IVtk_IShape.hxx" or
+    filename == "IVtk_IShapeData.hxx" or
+    filename == "IVtk_IShapeMesher.hxx" or
+    filename == "IVtk_IShapePickerAlgo.hxx" or
+    filename == "IVtkOCC_SelectableObject.hxx" or
+    filename == "IVtkOCC_Shape.hxx" or
+    filename == "IVtkOCC_ShapeMesher.hxx" or
+    filename == "IVtkOCC_ShapePickerAlgo.hxx" or
+    filename == "IVtk_IShapePickerAlgo.hxx" or
+    filename == "IVtkTools.hxx" or
+    filename == "IVtkTools_DisplayModeFilter.hxx" or
+    filename == "IVtkTools_ShapeDataSource.hxx" or
+    filename == "IVtkTools_ShapeObject.hxx" or
+    filename == "IVtkTools_ShapePicker.hxx" or
+    filename == "IVtkTools_SubPolyDataFilter.hxx" or
+    filename == "IVtkVTK_ShapeData.hxx"
+  ):
+    return False
+
+  # fatal error: 'vtkSmartPointer.h' file not found 
+  if (
+    filename == "IVtkVTK_View.hxx"
+  ):
+    return False
+
+  # fatal error: 'windows.h' file not found 
+  if (
+    filename == "OSD_WNT.hxx" or
+    filename == "WNT_Dword.hxx"
+  ):
+    return False
+
+  # fatal error: 'vtkActor.h' file not found
+  if filename == "IVtkDraw_HighlightAndSelectionPipeline.hxx":
+    return False
+
+  # error: expected member name or ';' after declaration specifiers
+  if filename == "math_Householder.hxx":
+    return False
+
+  return True
 
 # indicates if bindings for a class should be generated (returns True) or not (returns False)
 # parameters:
@@ -411,6 +487,26 @@ def processClass(theClass):
 
   # error: undefined symbol: _ZN21XCAFDoc_GeomToleranceC2ERKN11opencascade6handleIS_EE (referenced by top-level compiled C/C++ code)
   if theClass.spelling == "XCAFDoc_GeomTolerance":
+    return False
+
+  # error: expected unqualified-id
+  if (
+    theClass.spelling == "math_IntegerVector" or
+    theClass.spelling == "math_Matrix" or
+    theClass.spelling == "math_Vector"
+  ):
+    return False
+
+  # error: allocating an object of abstract class type 'AIS_Dimension'
+  if theClass.spelling == "AIS_Dimension":
+    return False
+
+  # error: calling a private constructor of class 'OpenGl_Clipping'
+  if theClass.spelling == "OpenGl_Context":
+    return False
+
+  # error: calling a private constructor of class 'OpenGl_Caps'
+  if theClass.spelling == "OpenGl_GraphicDriver":
     return False
 
   return True
@@ -830,6 +926,8 @@ def main():
         occtFiles.append(str(os.path.join(dirpath, item)))
 
   includePathArgs = [
+    "-I/emsdk/upstream/emscripten/system/include/",
+    "-I/opencascade/build/regal/regal-master/src/apitrace/thirdparty/khronos/",
     "-isystem/usr/lib/gcc/x86_64-linux-gnu/8/include-fixed/",
     "-I/usr/lib/gcc/x86_64-linux-gnu/8/include-fixed/",
     "-isystem", "/usr/lib/gcc/x86_64-linux-gnu/8/include/",
