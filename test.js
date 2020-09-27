445376b9fdcad71aca2ee20adaf0fbedf1b41714
Sebastian Alff <donalffons@gmail.com>
2020-09-27 02:44:38 +0200

    this should fail

--- test.js
+++ test.js
@@ -3,6 +3,7 @@ import opencascade from "./dist/opencascade.wasm.js";
 jest.setTimeout(300000);
 
 const BottleExample = async () => {
+  throw(new Error("This should fail, no?!"));
   const oc = await new opencascade();
 }
 
