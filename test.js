57f66b913daf356ec5f3e21fa7a2c9b0a7601565
Sebastian Alff <donalffons@gmail.com>
2020-09-27 21:43:23 +0200

    slight improvements to polygon test

--- test.js
+++ test.js
@@ -5,13 +5,14 @@ jest.setTimeout(300000);
 
 test('Create Polygonal Face', async () => {
   const oc = await((await new opencascade()).ready);
-  const path = [[0, 0, 0], [1, 0, 0], [1, 1, 0]].map(([x, y, z]) => new oc.gp_Pnt_3(x, y, z));
-  const makePolygon = new oc.BRepBuilderAPI_MakePolygon_1();
-  for (let nth = 0; nth < path.length; nth++) {
-    makePolygon.Add_1(path[nth]);
-  }
+  const builder = new oc.BRep_Builder();
+  const aComp = new oc.TopoDS_Compound();
+  builder.MakeCompound(aComp);
+  const path = [[0, 0, 0], [100, 0, 0], [100, 100, 0]].map(([x, y, z]) => new oc.gp_Pnt_3(x, y, z));
+  const makePolygon = new oc.BRepBuilderAPI_MakePolygon_3(path[0], path[1], path[2], true);
   const wire = makePolygon.Wire();
-  new oc.BRepBuilderAPI_MakeFace_15(wire, false);
+  const f = new oc.BRepBuilderAPI_MakeFace_15(wire, false);
+  builder.Add(aComp, f.Shape());
 });
 
 test('Bottle Example', async () => {
