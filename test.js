ea577c0dd1d9d1d7a2c16d3ab677302fe3b83a6b
Sebastian Alff <donalffons@gmail.com>
2020-09-27 21:20:33 +0200

    add another test

--- test.js
+++ test.js
@@ -3,6 +3,17 @@ import { makeBottle } from "opencascade.js-examples/src/demos/bottle - basic/lib
 
 jest.setTimeout(300000);
 
+test('Create Polygonal Face', async () => {
+  const oc = await((await new opencascade()).ready);
+  const path = [[0, 0, 0], [1, 0, 0], [1, 1, 0]].map(([x, y, z]) => new oc.gp_Pnt_3(x, y, z));
+  const makePolygon = new oc.BRepBuilderAPI_MakePolygon_1();
+  for (let nth = 0; nth < path.length; nth++) {
+    makePolygon.Add_1(path[nth]);
+  }
+  const wire = makePolygon.Wire();
+  new oc.BRepBuilderAPI_MakeFace_15(wire, false);
+});
+
 test('Bottle Example', async () => {
   const oc = await((await new opencascade()).ready);
   const width = 50, height = 70, thickness = 30;
