4c7a952925fbaec45475eeeba149ff313babdb92
Sebastian Alff <donalffons@gmail.com>
2020-09-28 08:55:17 +0200

    use library for makePolygon

--- test.js
+++ test.js
@@ -1,18 +1,12 @@
 import opencascade from "./dist/opencascade.wasm.js";
 import { makeBottle } from "opencascade.js-examples/src/demos/bottle - basic/library";
+import { makePolygon } from "opencascade.js-examples/src/demos/polygon/library";
 
 jest.setTimeout(300000);
 
 test('Create Polygonal Face', async () => {
   const oc = await((await new opencascade()).ready);
-  const builder = new oc.BRep_Builder();
-  const aComp = new oc.TopoDS_Compound();
-  builder.MakeCompound(aComp);
-  const path = [[0, 0, 0], [100, 0, 0], [100, 100, 0]].map(([x, y, z]) => new oc.gp_Pnt_3(x, y, z));
-  const makePolygon = new oc.BRepBuilderAPI_MakePolygon_3(path[0], path[1], path[2], true);
-  const wire = makePolygon.Wire();
-  const f = new oc.BRepBuilderAPI_MakeFace_15(wire, false);
-  builder.Add(aComp, f.Shape());
+  makePolygon(oc);
 });
 
 test('Bottle Example', async () => {
