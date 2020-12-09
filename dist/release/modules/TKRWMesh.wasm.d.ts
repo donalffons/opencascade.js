b373ee94fd3520990d000193b33ed3638ab96eb5
Sebastian Alff <donalffons@gmail.com>
2020-12-09 19:00:42 +0100

    some build updates

--- dist/release/modules/TKRWMesh.wasm.d.ts
+++ dist/release/modules/TKRWMesh.wasm.d.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1fb88028aec005b7062fc02c4b017a26d3eb4b5634d838695b76e6501ed04450
-size 17557
+oid sha256:45576f9ed3622633c8c01c40fa7c8dcab811ea6fdc1139eb0dfdf2fb87bd0503
+size 17585
--- dist/release/modules/TKernel.wasm.d.ts
+++ dist/release/modules/TKernel.wasm.d.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7da964c90eb833506abeda666a430955a77cfebd604139f8cc4e2312bd2bf9ed
-size 434028
+oid sha256:7708be68d631ff4f498274a98a7ecf7dca2caed20917504029e62fd23715aba2
+size 434621
