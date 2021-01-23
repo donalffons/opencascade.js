b72681f00e0a214ffe608072f70d8c6f415521a5
Sebastian Alff <donalffons@gmail.com>
2021-01-23 14:06:20 +0100

    move library compilation into docker build, skip OCCT build system

--- src/run.sh
+++ src/run.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-/opencascade.js/src/applyPatches.py
 if [ $# -eq 0 ]
 then
-  /opencascade.js/src/compile.py
   /opencascade.js/src/buildStandardModules.py
 else
   /opencascade.js/src/buildFromYaml.py "$@"
