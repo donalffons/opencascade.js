2ae45a5eec26b90e7858844ec719f160ec5b8a26
Sebastian Alff <donalffons@gmail.com>
2021-01-25 13:30:59 +0100

    reorganize files to avoid invalidating docker build cache all the time

--- src-build/applyPatches.py
+++ src-build/applyPatches.py
@@ -5,7 +5,7 @@ import subprocess
 
 os.chdir("/")
 
-for dirpath, dirnames, filenames in os.walk("/workspaces/opencascade.js/patches"):
+for dirpath, dirnames, filenames in os.walk("/opencascade.js/src-build/patches"):
   for filename in filenames:
     print("applying patch " + dirpath + "/" + filename)
     patchFile = open(dirpath + "/" + filename, 'r')
