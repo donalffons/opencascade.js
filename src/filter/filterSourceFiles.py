8b6e5050d20c125a5da0391012a27bc77aa24b8a
Sebastian Alff <donalffons@gmail.com>
2021-07-22 14:05:55 +0200

    make .mm files valid source files

--- src/filter/filterSourceFiles.py
+++ src/filter/filterSourceFiles.py
@@ -1,4 +1,4 @@
 def filterSourceFile(filename):
-  if filename.endswith(".cxx") or filename.endswith(".cpp") or filename.endswith(".c"):
+  if filename.endswith(".cxx") or filename.endswith(".cpp") or filename.endswith(".c") or filename.endswith(".mm"):
     return True
   return False
