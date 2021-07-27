d8e829fd0ce207a895b2957c0f1234dd2fca5bfc
Sebastian Alff <donalffons@gmail.com>
2021-07-27 23:52:56 +0200

    more exclusions ci_full

--- src/filter/filterPackages.py
+++ src/filter/filterPackages.py
@@ -3,42 +3,102 @@ def filterPackages(packageName):
     return False
 
   if packageName in [
-    "D3DHost",
-    "IVtk",
-    "IVtkVTK",
-    "IVtkTools",
-    "ViewerTest",
-    "IVtkOCC",
-    "XBRepMesh",
-    "XSDRAWSTLVRML",
-    "QADraw",
-    "XDEDRAW",
-    "TObjDRAW",
-    "SWDRAW",
-    "Draw",
-    "DPrsStd",
-    "BRepTest",
-    "MeshTest",
-    "GeomliteTest",
-    "DrawTrSurf",
-    "BOPTest",
-    "DrawFairCurve",
-    "DBRep",
-    "DNaming",
-    "GeometryTest",
-    "HLRTest",
-    "QANCollection",
-    "DDataStd",
-    "DrawDim",
-    "DDocStd",
-    "DDF",
-    "IVtkDraw",
-    "IVtk",
-    "IVtkOCC",
-    "IVtkVTK",
-    "IVtkTools",
-    "D3DHost",
-    "ViewerTest",
+    ## Full Module Draw
+      ## Toolkit TKDraw
+      "DBRep",
+      "Draw",
+      "DrawTrSurf",
+
+      ## Toolkit TKD3DHostTest
+      "D3DHostTest",
+
+      ## Toolkit TKIVtkDraw
+      "IVtkDraw",
+
+      ## Toolkit TKTopTest
+      "BOPTest",
+      "BRepTest",
+      "DrawFairCurve",
+      "GeometryTest",
+      "GeomliteTest",
+      "HLRTest",
+      "MeshTest",
+      "SWDRAW",
+
+      ## Toolkit TKViewerTest
+      "ViewerTest",
+
+      ## Toolkit TKOpenGlTest
+      "OpenGlTest",
+
+      ## Toolkit TKDCAF
+      "DDF",
+      "DDataStd",
+      "DDocStd",
+      "DNaming",
+      "DPrsStd",
+      "DrawDim",
+
+      ## Toolkit TKTObjDRAW
+      "TObjDRAW",
+
+      ## Toolkit TKXSDRAW
+      "XSDRAW",
+      "XSDRAWIGES",
+      "XSDRAWSTEP",
+      "XSDRAWSTLVRML",
+
+      ## Toolkit TKQADraw
+      "QABugs",
+      "QADNaming",
+      "QADraw",
+      "QANCollection",
+
+      ## Toolkit TKXDEDRAW
+      "XDEDRAW",
+
+    ## Partial Module Visualization
+      ## Toolkit TKD3DHost
+      "D3DHost",
+
+      ## Toolkit TKIVtk
+      "IVtk",
+      "IVtkOCC",
+      "IVtkTools",
+      "IVtkVTK",
+
+      ## Toolkit TKMeshVS
+      "MeshVS",
+
+      ## Toolkit TKOpenGl
+      ## Toolkit TKOpenGles
+      "OpenGl",
+
+      ## Toolkit TKService
+      "Aspect",
+      "Cocoa",
+      # "Font",
+      "Graphic3d",
+      "Image",
+      "Media",
+      "WNT",
+      # "Wasm",
+      # "Xw",
+
+      ## Toolkit TKV3d
+      "AIS",
+      "DsgPrs",
+      "Prs3d",
+      "PrsDim",
+      "PrsMgr",
+      # "Select3D",
+      # "SelectBasics",
+      # "SelectMgr",
+      "StdPrs",
+      # "StdSelect",
+      "V3d",
+
+    "XBRepMesh", # This leads to a naming clas with BRepMesh
   ]:
     return False
 
