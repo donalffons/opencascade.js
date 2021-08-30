f329a103a1c65ff34f7dbc1c22fe4ce1be3323c8
Sebastian Alff <donalffons@gmail.com>
2021-08-30 14:54:21 +0200

    add support for node js

---
+++ dist/node.js
@@ -0,0 +1,36 @@
+// patch __dirname
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+globalThis.__dirname = dirname(fileURLToPath(import.meta.url));
+
+// patch require
+import { createRequire } from 'module';
+globalThis.require = createRequire(import.meta.url);
+
+import ocFullJS from "./opencascade.full.js";
+
+const initOpenCascade = ({
+    mainJS = ocFullJS,
+    mainWasm = `${globalThis.__dirname}/opencascade.full.wasm`,
+    libs = [],
+    module = {},
+  } = {}) => {
+  return new Promise((resolve, reject) => {
+    new mainJS({
+      locateFile(path) {
+        if(path.endsWith('.wasm')) {
+          return mainWasm;
+        }
+        return path;
+      },
+      ...module
+    }).then(async oc => {
+      for(let lib of libs) {
+        await oc.loadDynamicLibrary(lib, {loadAsync: true, global: true, nodelete: true, allowUndefined: false});
+      }
+      resolve(oc);
+    });
+  });
+};
+
+export default initOpenCascade;
