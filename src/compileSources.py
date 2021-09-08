29d8b54cca835c60e37c6c78cf79f173ed016cab
Sebastian Alff <donalffons@gmail.com>
2021-09-08 23:41:47 +0200

    re-enable -O3 optimizations

--- src/compileSources.py
+++ src/compileSources.py
@@ -59,7 +59,7 @@ def buildObjectFiles(file):
     "-DOCCT_NO_PLUGINS",
     "-frtti",
     "-DHAVE_RAPIDJSON", 
-    "-Os",
+    "-O3",
     # "-g3",
     # "-gsource-map",
     # "--source-map-base=http://localhost:8080",
