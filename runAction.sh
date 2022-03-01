10e832e9256efc9b0ea32016802bdffec647c726
Sebastian Alff <donalffons@gmail.com>
2022-03-01 22:21:11 +0100

    use latest gh runner binaries

--- runAction.sh
+++ runAction.sh
@@ -49,8 +49,12 @@ GH_ACTION_TOKEN_RESPONSE=$(curl -u opencascade.js-build:$GH_API_TOKEN   -X POST 
 GH_ACTION_TOKEN=$(echo $GH_ACTION_TOKEN_RESPONSE | jq -r ".token")
 
 mkdir actions-runner && cd actions-runner
-curl -o actions-runner-linux-x64-2.283.1.tar.gz -L https://github.com/actions/runner/releases/download/v2.283.1/actions-runner-linux-x64-2.283.1.tar.gz
-tar xzf ./actions-runner-linux-x64-2.283.1.tar.gz
+RUNNERURL=$(curl -s https://api.github.com/repos/actions/runner/releases/latest \
+| grep -E "browser_download_url\": \".*download.*linux-x64-[^-]+.tar.gz" \
+| cut -d : -f 2,3 \
+| tr -d \")
+curl -o actions-runner-linux.tar.gz -L $RUNNERURL
+tar xzf ./actions-runner-linux.tar.gz
 ./config.sh --unattended --url https://github.com/$PROJECT_NAME --token $GH_ACTION_TOKEN --replace --name opencascade-js-build-$UUID --labels $LABEL
 ./run.sh --once || true
 sleep 60
