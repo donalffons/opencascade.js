ce97ef112805dba3e342ccd34012082edad79925
Sebastian Alff <donalffons@gmail.com>
2022-03-15 14:18:18 +0100

    fixed incorrect folders in createPatch script

--- src/createPatch.sh
+++ src/createPatch.sh
@@ -3,7 +3,7 @@ rm -rf /occt-original || true
 mkdir /occt-original
 cd /occt-original
 tar -xvf /occt/occt.tar.gz -C .
-mv /occt/occt-$OCCT_COMMIT_HASH/* .
-mv /occt/occt-$OCCT_COMMIT_HASH/.* . || true
+mv /occt-original/occt-$OCCT_COMMIT_HASH/* .
+mv /occt-original/occt-$OCCT_COMMIT_HASH/.* . || true
 
-diff -ruN /occt-original/ /occt/occt/ > /opencascade.js/src_build/patches/newPatch.patch
+diff -ruN /occt-original/ /occt/ > /opencascade.js/src/patches/newPatch.patch
