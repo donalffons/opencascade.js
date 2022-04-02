24501e18360649d4ab162f2a58aa6805c34e690d
Sebastian Alff <donalffons@gmail.com>
2022-04-02 18:30:13 +0200

    fix copyright

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -109,7 +109,7 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} OpenCascade.js`,
       },
       prism: {
         theme: lightCodeTheme,
