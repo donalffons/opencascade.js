39b73dde77525c6e09198f2c398d1b92ba7908ff
Sebastian Alff <donalffons@gmail.com>
2022-04-04 21:41:39 +0200

    simplify

--- website/ocjs-editor-theme/src/theme/CodeBlock/opencascade.worker.ts
+++ website/ocjs-editor-theme/src/theme/CodeBlock/opencascade.worker.ts
@@ -7,19 +7,13 @@ export default {} as typeof Worker & { new(): Worker };
 export async function runOCJSCode(code: string): Promise<Uint8Array | undefined> {
   const oc = await initOpenCascade();
   const visualize_ = (shape: TopoDS_Shape) => {
-    const app = new oc.TDocStd_Application();
-    oc.BinXCAFDrivers.DefineFormat(new oc.Handle_TDocStd_Application_2(app));
-
-    const docHandle = new oc.Handle_TDocStd_Document_1();
-    app.NewDocument_2(new oc.TCollection_ExtendedString_2("BinXCAF", true), docHandle);
-    const doc = docHandle.get();
-    const st = oc.XCAFDoc_DocumentTool.ShapeTool(doc.Main()).get();
-    const shapeLabel = st.NewShape();
-    st.SetShape(shapeLabel, shape);
+    const docHandle = new oc.Handle_TDocStd_Document_2(new oc.TDocStd_Document(new oc.TCollection_ExtendedString_1()));
+    const shapeTool = oc.XCAFDoc_DocumentTool.ShapeTool(docHandle.get().Main()).get();
+    shapeTool.SetShape(shapeTool.NewShape(), shape);
 
     new oc.BRepMesh_IncrementalMesh_2(shape, 0.1, false, 0.1, false);
     const cafWriter = new oc.RWGltf_CafWriter(new oc.TCollection_AsciiString_2("./file.glb"), true);
-    cafWriter.Perform_2(docHandle, new oc.TColStd_IndexedDataMapOfStringString_1(), new oc.Message_ProgressRange_1())
+    cafWriter.Perform_2(docHandle, new oc.TColStd_IndexedDataMapOfStringString_1(), new oc.Message_ProgressRange_1());
 
     return oc.FS.readFile("./file.glb", { encoding: "binary" });
   };
