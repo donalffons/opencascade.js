1a56b5d373f491a0cffd8ea441c66d280d0ec59a
Sebastian Alff <donalffons@gmail.com>
2022-04-12 22:36:36 +0200

    add image alt tag

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -11,7 +11,7 @@ function HomepageHeader() {
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
-        <img className={styles.heroImage} src="/img/logo.svg" />
+        <img className={styles.heroImage} src="/img/logo.svg" alt="OpenCascade.js Logo" />
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
