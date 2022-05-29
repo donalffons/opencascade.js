5c4d387ab51663b6edd86a75b61a67b0b2145c2b
Mark van der Net <mark@oscity.nl>
2022-05-29 15:25:55 +0200

    Added template for nuxt-bridge and vite

---
+++ starter-templates/ocjs-create-nuxt-app-bridge-vite/pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <OcApp/>
+</template>
+
+<script>
+
+import OcApp from '~/components/OcApp'
+
+export default {
+  name: 'IndexPage',
+  components: {
+    OcApp
+  },
+
+  data(){
+    return { }
+  },
+
+  mounted()
+  {
+   
+  }
+}
+</script>
